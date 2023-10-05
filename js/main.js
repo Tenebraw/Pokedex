66fbf0dec43eff3ad7c7df108f8524aa78b37b7d
Marcos A. T <32946589+Tenebraw@users.noreply.github.com>
2023-10-05 03:10:29 -0300

    Instalattion of Eslint
    Instalacion de Eslint y tambien cambio en las funciones Next, Previous y Search, especialmente en su diseño.

--- js/main.js
+++ js/main.js
@@ -1,119 +1,113 @@
 /// <reference types="jquery"/>
+/* eslint-env jquery */
 
-modalStructure();
-
-let actualPage = $("#actualpage");
-let arrayRequests = [];
-let i = 0;
+const actualPage = $('#actualpage');
+const arrayRequests = [];
+const i = 0;
 let page = 1;
 
-for (let i = 0; i <= 1260; i = i + 20) {
-  arrayRequests.push(`https://pokeapi.co/api/v2/pokemon/?offset=${i}&limit=20`);
+for (let j = 0; j <= 1260; j += 20) {
+  arrayRequests.push(`https://pokeapi.co/api/v2/pokemon/?offset=${j}&limit=20`);
 }
-
 const UrlPokemon = arrayRequests[i];
-makingRequest(UrlPokemon);
 
 function makingRequest(Url) {
   fetch(Url)
     .then((response) => response.json())
     .then((response) => {
-      for (let i = 0; i < $(".card-title").length; i++) {
-        $($(".card-title")[i]).text(response.results[i].name);
+      for (let k = 0; k < $('.card-title').length; k++) {
+        $($('.card-title')[k]).text(response.results[k].name);
 
-        let secondFetch = response.results[i].url;
+        const secondFetch = response.results[k].url;
         fetch(secondFetch)
           .then((secondresp) => secondresp.json())
           .then((secondresp) => {
-            $($(".card-img-top")[i]).attr(
-              "src",
-              secondresp.sprites.front_default
-            );
-            $($(".modal-title")[i]).text(secondresp.name);
-            $($(".front-default")[i]).attr(
-              "src",
-              secondresp.sprites.front_default
-            );
-            $($(".back-default")[i]).attr(
-              "src",
-              secondresp.sprites.back_default
-            );
-            $($(".shiny-front")[i]).attr("src", secondresp.sprites.front_shiny);
-            $($(".shiny-back")[i]).attr("src", secondresp.sprites.back_shiny);
+            $($('.card-img-top')[k]).attr('src', secondresp.sprites.front_default);
+            $($('.modal-title')[k]).text(secondresp.name);
+            $($('.front-default')[k]).attr('src', secondresp.sprites.front_default);
+            $($('.back-default')[k]).attr('src', secondresp.sprites.back_default);
+            $($('.shiny-front')[k]).attr('src', secondresp.sprites.front_shiny);
+            $($('.shiny-back')[k]).attr('src', secondresp.sprites.back_shiny);
 
-            if (secondresp.types.length == 1) {
-              $($(".tipo1")[i]).text(secondresp.types[0].type.name);
+            if (secondresp.types.length === 1) {
+              $($('.tipo1')[k]).text(secondresp.types[0].type.name);
             } else {
-              $($(".tipo1")[i]).text(secondresp.types[0].type.name);
-              $($(".tipo2")[i]).text(secondresp.types[1].type.name);
+              $($('.tipo1')[k]).text(secondresp.types[0].type.name);
+              $($('.tipo2')[k]).text(secondresp.types[1].type.name);
             }
-            $($(".health")[i]).text(secondresp.stats[0].base_stat);
-            $($(".attack")[i]).text(secondresp.stats[1].base_stat);
-            $($(".defense")[i]).text(secondresp.stats[2].base_stat);
-            $($(".specialattack")[i]).text(secondresp.stats[3].base_stat);
-            $($(".specialdefense")[i]).text(secondresp.stats[4].base_stat);
-            $($(".speed")[i]).text(secondresp.stats[5].base_stat);
+            $($('.health')[k]).text(secondresp.stats[0].base_stat);
+            $($('.attack')[k]).text(secondresp.stats[1].base_stat);
+            $($('.defense')[k]).text(secondresp.stats[2].base_stat);
+            $($('.specialattack')[k]).text(secondresp.stats[3].base_stat);
+            $($('.specialdefense')[k]).text(secondresp.stats[4].base_stat);
+            $($('.speed')[k]).text(secondresp.stats[5].base_stat);
           })
-          .catch((error) => console.log("Error!", error));
+          .catch((error) => console.log('Error!', error));
       }
     })
     .catch((error) => {
-      console.log("Error!", error);
+      console.log('Error!', error);
     });
 }
 
-function nextPokemonList() {
-  if (page == 63) {
-    $("#next").addClass("disabled");
-  }
+// Manage Next action.
 
+$('#next').on('click', () => {
+  if (page === 63) {
+    $('#next').addClass('disabled');
+  }
   page++;
   actualPage.val(page);
-  if (page == 2) {
-    $("#previous").removeClass("disabled");
+  if (page === 2) {
+    $('#previous').removeClass('disabled');
   }
-  makingRequest(`${arrayRequests[page]}`);
-}
+  makingRequest(`${arrayRequests[page - 1]}`);
+});
 
-function previousPokemonList() {
+// Manage Previous action.
+
+$('#previous').on('click', () => {
   page--;
   actualPage.val(page);
-  $("#next").removeClass("disabled");
-  if (page == 1) {
-    $("#previous").addClass("disabled");
+  $('#next').removeClass('disabled');
+  if (page === 1) {
+    $('#previous').addClass('disabled');
   }
   makingRequest(arrayRequests[page - 1]);
-}
+});
 
-function search(ele) {
-  if (event.key === "Enter") {
-    if (validarInput(ele) == true) {
-      actualPage.val(ele.value);
-      let newId = parseInt(ele.value) - 1;
-      makingRequest(arrayRequests[newId]);
-      page = ele.value;
-      $("#previous").removeClass("disabled");
-      if (page == 64) {
-        $("#next").addClass("disabled");
-      }
-    }
-  }
-}
+// Validate Input Search.
 
-function validarInput(ele) {
-  $("#pagego").removeClass("error");
-  $("#error").css("display", "none");
+function validarInput() {
+  $('#pagego').removeClass('error');
+  $('#error').css('display', 'none');
 
-  if (!$("#pagego").val().match(/^\d+$/) || ele.value > 64 || ele.value < 1) {
-    $("#pagego").addClass("error");
-    $("#error").css("display", "block");
+  if (!$('#pagego').val().match(/^\d+$/) || $('#pagego').val() > 64 || $('#pagego').val() < 1) {
+    $('#pagego').addClass('error');
+    $('#error').css('display', 'block');
     return false;
   }
   return true;
 }
 
+// Manage search input
+$('#pagego').on('keydown', (e) => {
+  if (e.key === 'Enter') {
+    if (validarInput() === (true)) {
+      actualPage.val($('#pagego').val());
+      const newId = parseInt($('#pagego').val(), 10) - 1;
+      makingRequest(arrayRequests[newId]);
+      page = $('#pagego').val();
+      $('#previous').removeClass('disabled');
+      if (parseInt(page, 10) === 64) {
+        $('#next').addClass('disabled');
+      }
+    }
+  }
+});
+
 function modalStructure() {
-  $(".col").after(`    <!-- Modal -->
+  $('.col').after(`    <!-- Modal -->
 <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -144,9 +138,12 @@ function modalStructure() {
 </div>
 <!--End modal-->`);
 
-  let idsModal = document.querySelectorAll("#Modal");
+  const idsModal = document.querySelectorAll('#Modal');
 
-  for (let i = 0; i < idsModal.length; i++) {
-    idsModal[i].setAttribute("id", `modal${i + 1}`);
+  for (let l = 0; l < idsModal.length; l++) {
+    idsModal[l].setAttribute('id', `modal${l + 1}`);
   }
 }
+
+modalStructure();
+makingRequest(UrlPokemon);
